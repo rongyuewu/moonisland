c93a3c77b4da43dd48aeb3a5546f4acdba261d27
Axian94 <32283200+Axian94@users.noreply.github.com>
2017-09-27 17:49:20 +0800

    goodspic

---
+++ src/cn/com/yld/service/GoodsPicService.java
@@ -0,0 +1,19 @@
+package cn.com.yld.service;
+
+import java.util.List;
+import java.util.Map;
+
+import cn.com.yld.pojo.GoodsPic;
+
+public interface GoodsPicService {
+	//查询全部
+		public List<GoodsPic> findall();
+		//id查询
+		public List<GoodsPic> findbyid(GoodsPic goodsPic);
+		//分页查询
+		public List<GoodsPic> findlinmit(Map<String, Integer> map);
+		//插入
+		public void insert(GoodsPic goodsPic);
+		//更新
+		public void update(GoodsPic goodsPic);
+}
