547c08ea80c12b4c1128254412a572d5d9dce1f2
OldAn <31846963+OldAn@users.noreply.github.com>
2017-09-27 17:45:30 +0800

    order相关事务

--- src/cn/com/yld/mapper/OrderMapper.java
+++ src/cn/com/yld/mapper/OrderMapper.java
@@ -6,6 +6,7 @@ import cn.com.yld.pojo.Order;
 
 public interface OrderMapper {
 	public List<Order> findorderstatus();
-	public List<String> findordernumber();
+	public List<Order> findordernumber();
 	public void updatederstatus(Order order);
+	public List<Order> findorder(Order order);
 }
