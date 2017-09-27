17796310e2f2ad6ba792027b0b547a890449c4b0
周文琦 <32192239+ArroganceMo@users.noreply.github.com>
2017-09-27 20:19:06 +0800

    save Address

--- src/cn/com/yld/mapper/SaveMapper.java
+++ src/cn/com/yld/mapper/SaveMapper.java
@@ -1,19 +1,20 @@
 package cn.com.yld.mapper;
 import java.util.List;
+import java.util.Map;
 
 import cn.com.yld.pojo.Save;
 
 public interface SaveMapper {
 	// 查询全部
-	public Save find(Save save);
+	public List<Save> findall();
 	// id查询
-	public List<Save> findbyid();
+	public List<Save> findbyid(Save save);
 	// 插入
 	public void insert(Save save);
-
 	// 更新
 	public void update(Save save);
-
+	//总页数
+	public int pagecount();
 	// 分页
-	public List<Save> findbylike();
+	public List<Save> findbylike(Map<String, Integer> map);
 }
